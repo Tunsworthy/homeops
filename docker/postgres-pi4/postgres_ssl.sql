30d9a831a1c9c06da169a25e7b4d128069570e83
Tom <mail@tomunsworth.net>
2026-01-06 21:33:19 +1100

    updated postgres

--- docker/postgres-pi4/postgres_ssl.sql
+++ docker/postgres-pi4/postgres_ssl.sql
@@ -1,6 +1,7 @@
 -- Postgres SSL configuration applied during initialization
 -- This script will be executed by the official postgres image during init
 ALTER SYSTEM SET ssl = 'on';
-ALTER SYSTEM SET ssl_cert_file = 'server.crt';
-ALTER SYSTEM SET ssl_key_file = 'server.key';
+-- Use absolute paths to the certs mounted from the `postgres_certs` volume
+ALTER SYSTEM SET ssl_cert_file = '/var/lib/postgresql/certs/server.crt';
+ALTER SYSTEM SET ssl_key_file = '/var/lib/postgresql/certs/server.key';
 SELECT pg_reload_conf();
