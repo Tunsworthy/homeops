feb38c549847071ff9e0544ac9a15d110adf8144
Tom <mail@tomunsworth.net>
2026-02-04 15:03:15 +1100

    postgres updates

--- docker/postgres-pi4/init_db.sql
+++ docker/postgres-pi4/init_db.sql
@@ -4,20 +4,20 @@
 -- /docker-entrypoint-initdb.d on database initialization.
 
 -- Idempotent creation of `housefinder` role and database.
-CREATE ROLE IF NOT EXISTS housefinder WITH LOGIN ENCRYPTED PASSWORD '{{ housefinder_db_password | default("change_me_replace") }}';
-CREATE DATABASE IF NOT EXISTS housefinder OWNER housefinder;
+CREATE ROLE housefinder WITH LOGIN ENCRYPTED PASSWORD '{{ housefinder_db_password | default("change_me_replace") }}';
+CREATE DATABASE housefinder OWNER housefinder;
 GRANT ALL PRIVILEGES ON DATABASE housefinder TO housefinder;
 
 -- Idempotent creation of `babytracker` role and database.
-CREATE ROLE IF NOT EXISTS babytracker WITH LOGIN ENCRYPTED PASSWORD '{{ babytracker_db_password | default("change_me_replace") }}';
-CREATE DATABASE IF NOT EXISTS babytracker OWNER babytracker;
+CREATE ROLE babytracker WITH LOGIN ENCRYPTED PASSWORD '{{ babytracker_db_password | default("change_me_replace") }}';
+CREATE DATABASE babytracker OWNER babytracker;
 GRANT ALL PRIVILEGES ON DATABASE babytracker TO babytracker;
 
 -- Idempotent creation of `babytracker_test` role and database (empty - data cloned separately).
-CREATE ROLE IF NOT EXISTS babytracker_test WITH LOGIN ENCRYPTED PASSWORD '{{ babytracker_test_db_password | default("change_me_replace") }}';
-CREATE DATABASE IF NOT EXISTS babytracker_test OWNER babytracker_test;
+CREATE ROLE babytracker_test WITH LOGIN ENCRYPTED PASSWORD '{{ babytracker_test_db_password | default("change_me_replace") }}';
+CREATE DATABASE babytracker_test OWNER babytracker_test;
 GRANT ALL PRIVILEGES ON DATABASE babytracker_test TO babytracker_test;
 
-CREATE ROLE IF NOT EXISTS babytracker_prod WITH LOGIN ENCRYPTED PASSWORD '{{ babytracker_prod_db_password | default("change_me_replace") }}';
-CREATE DATABASE IF NOT EXISTS babytracker_prod OWNER babytracker_prod;
+CREATE ROLE babytracker_prod WITH LOGIN ENCRYPTED PASSWORD '{{ babytracker_prod_db_password | default("change_me_replace") }}';
+CREATE DATABASE babytracker_prod OWNER babytracker_prod;
 GRANT ALL PRIVILEGES ON DATABASE babytracker_prod TO babytracker_prod;
